c4a182c19557a25492a1421cb745ccb5dd8404e7
Mirus <lomirus.dev@protonmail.com>
2023-06-04 00:02:09 +0800

    Basic layout for Settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -10,6 +10,41 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("WIP"));
+    return Column(children: [
+      ListTile(
+        leading: const Icon(Icons.cloud_download),
+        title: const Text("Fetch Lexicon JSON"),
+        subtitle: const Text("Recommended"),
+        onTap: () => showDialog(
+          context: context,
+          builder: (BuildContext context) => const AlertDialog(
+            title: Text("WIP"),
+          ),
+        ),
+      ),
+      ListTile(
+        leading: const Icon(Icons.upload_file),
+        title: const Text("Import Lexicon JSON"),
+        subtitle: const Text("Alternative for users with Internet issues"),
+        onTap: () => showDialog(
+          context: context,
+          builder: (BuildContext context) => const AlertDialog(
+            title: Text("WIP"),
+          ),
+        ),
+      ),
+      const Divider(),
+      ListTile(
+        leading: const Icon(Icons.info),
+        title: const Text("About"),
+        subtitle: const Text("1.0.0 (dev)"),
+        onTap: () => showDialog(
+          context: context,
+          builder: (BuildContext context) => const AlertDialog(
+            title: Text("WIP"),
+          ),
+        ),
+      ),
+    ]);
   }
 }
