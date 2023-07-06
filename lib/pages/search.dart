ba029cf1e1669501d5dd5f264e4d597c770f48e0
Mirus <lomirus.dev@protonmail.com>
2023-07-06 23:39:12 +0800

    Update search bar UI

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -71,44 +71,54 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        const SliverToBoxAdapter(
-          child: SizedBox(height: 12),
-        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 12)),
         SliverAppBar(
-          clipBehavior: Clip.none,
-          shape: const StadiumBorder(),
-          // scrolledUnderElevation: 0.0,
-          // titleSpacing: 0.0,
-          title: Consumer<LexiconModel>(
-            builder: (context, lexicon, child) {
-              return SearchAnchor.bar(
-                searchController: controller,
-                barHintText: "Search roots...",
-                suggestionsBuilder: (BuildContext context, SearchController controller) async {
-                  final result = await search(lexicon.database);
-                  return result.map(
-                    (root) => ListTile(
-                      leading: const Icon(Icons.article),
-                      title: Text(root.root),
-                      subtitle: Text(
-                        root.refers ?? '',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+          title: Container(
+            margin: const EdgeInsets.all(12),
+            child: const Text("Search"),
+          ),
+          actions: [
+            Container(
+              margin: const EdgeInsets.all(12),
+              child: Consumer<LexiconModel>(
+                builder: (context, lexicon, child) {
+                  return SearchAnchor(
+                    searchController: controller,
+                    suggestionsBuilder: (BuildContext context, SearchController controller) async {
+                      final result = await search(lexicon.database);
+                      return result.map(
+                        (root) => ListTile(
+                          leading: const Icon(Icons.article),
+                          title: Text(root.root),
+                          subtitle: Text(
+                            root.refers ?? '',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSurfaceVariant,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => RootPage(root)),
+                            );
+                          },
                         ),
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => RootPage(root)),
-                        );
-                      },
-                    ),
+                      );
+                    },
+                    builder: (BuildContext context, SearchController controller) {
+                      return IconButton(
+                        onPressed: () {
+                          controller.openView();
+                        },
+                        icon: const Icon(Icons.search),
+                      );
+                    },
                   );
                 },
-              );
-            },
-          ),
+              ),
+            ),
+          ],
         ),
         SliverToBoxAdapter(
           child: Container(
