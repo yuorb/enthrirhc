d237c2d7f9177112deaf3544b7ecc7742fba1ef7
Mirus <lomirus.dev@protonmail.com>
2023-07-06 23:18:47 +0800

    Update search page UI

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:enthrirch/pages/root.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +8,28 @@ import 'package:enthrirch/common/store.dart';
 
 import '../common/types.dart';
 import '../database/shared.dart';
+
+class LexiconActionButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+  const LexiconActionButton(
+      {super.key, required this.icon, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton.tonalIcon(
+      onPressed: onPressed,
+      style: FilledButton.styleFrom(
+        minimumSize: const Size.fromHeight(64),
+        alignment: const Alignment(-1, 0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      ),
+      label: Text("   $label"),
+      icon: Icon(icon),
+    );
+  }
+}
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -87,12 +110,115 @@ class _SearchPageState extends State<SearchPage> {
             },
           ),
         ),
-        const SliverToBoxAdapter(
-          child: Column(
-            children: [
-              SizedBox(height: 256),
-              Text("Work In Progress"),
-            ],
+        SliverToBoxAdapter(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: FilledButton.icon(
+                        onPressed: null,
+                        style: FilledButton.styleFrom(
+                          minimumSize: const Size.fromHeight(64),
+                          alignment: const Alignment(-1, 0),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+                        ),
+                        label: const Text("Root: WIP"),
+                        icon: const Icon(Icons.article),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: FilledButton.tonalIcon(
+                        onPressed: null,
+                        style: FilledButton.styleFrom(
+                          minimumSize: const Size.fromHeight(64),
+                          alignment: const Alignment(-1, 0),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+                        ),
+                        label: const Text("Affixes: 0"),
+                        icon: const Icon(Icons.article),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 8),
+                LexiconActionButton(
+                  icon: Icons.cloud_download,
+                  label: "Fetch lexicon from Github repository",
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (BuildContext context) => const AlertDialog(
+                      title: Text("WIP"),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                LexiconActionButton(
+                  icon: Icons.upload_file,
+                  label: "Import lexicon from local files",
+                  onPressed: () async {
+                    FilePickerResult? result = await FilePicker.platform.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['json'],
+                      withData: true,
+                    );
+
+                    if (result == null) return;
+                    final bytes = result.files.single.bytes;
+                    if (bytes == null) return;
+                    final text = utf8.decode(bytes);
+                    List<dynamic> decodedJson;
+                    try {
+                      decodedJson = jsonDecode(text);
+                    } catch (e) {
+                      if (context.mounted) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text("Error"),
+                            content: Text("Invalid JSON file (${e.runtimeType})"),
+                          ),
+                        );
+                      }
+                      return;
+                    }
+                    List<Root> lexicon = decodedJson.map((root) => Root.fromJson(root)).toList();
+                    if (context.mounted) {
+                      await Provider.of<LexiconModel>(context, listen: false)
+                          .database
+                          .init(lexicon);
+                    }
+                    if (context.mounted) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text("Success"),
+                          content: Text(
+                            "Imported ${lexicon.length.toString()} roots successfully.",
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(height: 8),
+                LexiconActionButton(
+                  icon: Icons.delete_forever,
+                  label: "Clear the lexicon",
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (BuildContext context) => const AlertDialog(
+                      title: Text("WIP"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         )
       ],
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,12 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:enthrirch/pages/communities.dart';
-import 'package:provider/provider.dart';
-
-import '../common/store.dart';
-import '../common/types.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -22,81 +15,6 @@ class _SettingsPageState extends State<SettingsPage> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         AppBar(title: const Text("Settings")),
-        Container(
-          padding: const EdgeInsets.fromLTRB(16, 24, 0, 8),
-          child: Text(
-            "Import Lexicon",
-            style: TextStyle(color: Theme.of(context).colorScheme.primary),
-          ),
-        ),
-        ListTile(
-          leading: const Icon(Icons.cloud_download),
-          title: const Text("Github"),
-          subtitle: Text(
-            "Fetch from the repository on Github",
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onSurfaceVariant,
-            ),
-          ),
-          onTap: () => showDialog(
-            context: context,
-            builder: (BuildContext context) => const AlertDialog(
-              title: Text("WIP"),
-            ),
-          ),
-        ),
-        ListTile(
-          leading: const Icon(Icons.upload_file),
-          title: const Text("File"),
-          subtitle: Text(
-            "Import from local files",
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onSurfaceVariant,
-            ),
-          ),
-          onTap: () async {
-            FilePickerResult? result = await FilePicker.platform.pickFiles(
-              type: FileType.custom,
-              allowedExtensions: ['json'],
-              withData: true,
-            );
-
-            if (result == null) return;
-            final bytes = result.files.single.bytes;
-            if (bytes == null) return;
-            final text = utf8.decode(bytes);
-            List<dynamic> decodedJson;
-            try {
-              decodedJson = jsonDecode(text);
-            } catch (e) {
-              if (context.mounted) {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("Error"),
-                    content: Text("Invalid JSON file (${e.runtimeType})"),
-                  ),
-                );
-              }
-              return;
-            }
-            List<Root> lexicon = decodedJson.map((root) => Root.fromJson(root)).toList();
-            if (context.mounted) {
-              await Provider.of<LexiconModel>(context, listen: false).database.init(lexicon);
-            }
-            if (context.mounted) {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text("Success"),
-                  content: Text(
-                    "Imported ${lexicon.length.toString()} roots successfully.",
-                  ),
-                ),
-              );
-            }
-          },
-        ),
         Container(
           padding: const EdgeInsets.fromLTRB(16, 24, 0, 8),
           child: Text(
