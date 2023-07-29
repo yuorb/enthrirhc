30a29ac1fdcab7980427d1ae70f5a53ca2d6b696
Mirus <lomirus.dev@protonmail.com>
2023-07-30 03:18:53 +0800

    Check width of a/c anchors for primary

--- lib/common/character/primary/utils.dart
+++ lib/common/character/primary/utils.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 import 'package:enthrirch/common/character/mod.dart';
 import 'package:enthrirch/common/utils.dart';
 
+import 'a_anchor.dart';
 import 'b_anchor.dart';
+import 'c_anchor.dart';
 import 'd_anchor.dart';
 
 (double, double) getAnchorBoundary(String path) {
@@ -24,21 +26,29 @@ import 'd_anchor.dart';
 }
 
 (double, double) getPrimaryBoundary(Primary primary) {
+  final aAnchorPath = aAnchorData[primary.essence.name]![primary.affiliation.name]!;
   final bAnchorPath = bAnchorData[primary.perspective.name]![primary.extension.name]!;
+  final cAnchorPath = cAnchorData[primary.similarity.name]![primary.separability.name]!;
   final dAnchorPath = dAnchorData[primary.function.name]![primary.version.name]![
       primary.plexity.name]![primary.stem.name]!;
   final (coreLeft, coreRight) = getCoreBoundary(primary.specification.path);
+  final (aAnchorLeft, aAnchorRight) = getAnchorBoundary(aAnchorPath);
   final (bAnchorLeft, bAnchorRight) = getAnchorBoundary(bAnchorPath);
+  final (cAnchorLeft, cAnchorRight) = getAnchorBoundary(cAnchorPath);
   final (dAnchorLeft, dAnchorRight) = getAnchorBoundary(dAnchorPath);
 
   final left = [
     coreLeft,
+    primary.specification.aAnchor.x + aAnchorLeft,
     primary.specification.bAnchor.x + bAnchorLeft,
+    primary.specification.cAnchor.x + cAnchorLeft,
     primary.specification.dAnchor.x + dAnchorLeft,
   ].reduce(min);
   final right = [
     coreRight,
+    primary.specification.aAnchor.x + aAnchorRight,
     primary.specification.bAnchor.x + bAnchorRight,
+    primary.specification.cAnchor.x + cAnchorRight,
     primary.specification.dAnchor.x + dAnchorRight,
   ].reduce(max);
 
