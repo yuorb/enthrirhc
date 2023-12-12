91d01e8c005562780b56a37754bf01a6986b6f0c
Mirus <lomirus.dev@protonmail.com>
2023-12-12 23:47:46 +0800

    Fix web download

--- lib/libs/download/web.dart
+++ lib/libs/download/web.dart
@@ -3,7 +3,8 @@ import 'dart:convert' show utf8;
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' show AnchorElement;
 
-Future<void> saveTextFile(String text, String filename) async {
+// ignore: body_might_complete_normally_nullable
+Future<String?> saveTextFile(String text, String filename) async {
   AnchorElement()
     ..href = '${Uri.dataFromString(text, mimeType: 'text/plain', encoding: utf8)}'
     ..download = filename
