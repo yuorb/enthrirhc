61637f7462e7ff63e705151f5e08735221036ce2
Mirus <lomirus.dev@protonmail.com>
2023-12-12 14:53:11 +0800

    Improve "Change Degree" dialog style

--- lib/pages/construct/degree_dialog.dart
+++ lib/pages/construct/degree_dialog.dart
@@ -6,70 +6,73 @@ Future<Degree?> showDegreeDialog(BuildContext context, Degree initialValue) {
     context: context,
     builder: (context) {
       Degree selectedValue = initialValue;
+      ScrollController scrollController = ScrollController();
       return StatefulBuilder(
         builder: (context, setState) => AlertDialog(
           title: const Text("Change Degree"),
           content: SizedBox(
-            width: double.maxFinite,
-            child: ListView(
-              // mainAxisSize: MainAxisSize.min,
-              children: [
-                {
-                  "value": Degree.d1,
-                  "title": "Degree 1",
-                },
-                {
-                  "value": Degree.d2,
-                  "title": "Degree 2",
-                },
-                {
-                  "value": Degree.d3,
-                  "title": "Degree 3",
-                },
-                {
-                  "value": Degree.d4,
-                  "title": "Degree 4",
-                },
-                {
-                  "value": Degree.d5,
-                  "title": "Degree 5",
-                },
-                {
-                  "value": Degree.d6,
-                  "title": "Degree 6",
-                },
-                {
-                  "value": Degree.d7,
-                  "title": "Degree 7",
-                },
-                {
-                  "value": Degree.d8,
-                  "title": "Degree 8",
-                },
-                {
-                  "value": Degree.d9,
-                  "title": "Degree 9",
-                },
-                {
-                  "value": Degree.d0,
-                  "title": "Degree 0",
-                },
-              ].map((e) {
-                final Degree value = e['value'] as Degree;
-                final String title = e['title'] as String;
-                return RadioListTile(
-                  value: value,
-                  groupValue: selectedValue,
-                  title: Text(title),
-                  onChanged: (degree) {
-                    if (degree != null) {
-                      setState(() {
-                        selectedValue = degree;
-                      });
-                    }
+            width: 320,
+            height: 320,
+            child: Scrollbar(
+              controller: scrollController,
+              child: ListView(
+                // mainAxisSize: MainAxisSize.min,
+                controller: scrollController,
+                children: [
+                  {
+                    "value": Degree.d1,
+                    "title": "Degree 1",
                   },
-                );
-              }).toList(),
+                  {
+                    "value": Degree.d2,
+                    "title": "Degree 2",
+                  },
+                  {
+                    "value": Degree.d3,
+                    "title": "Degree 3",
+                  },
+                  {
+                    "value": Degree.d4,
+                    "title": "Degree 4",
+                  },
+                  {
+                    "value": Degree.d5,
+                    "title": "Degree 5",
+                  },
+                  {"value": Degree.d6, "title": "Degree 6"},
+                  {
+                    "value": Degree.d7,
+                    "title": "Degree 7",
+                  },
+                  {
+                    "value": Degree.d8,
+                    "title": "Degree 8",
+                  },
+                  {
+                    "value": Degree.d9,
+                    "title": "Degree 9",
+                  },
+                  {
+                    "value": Degree.d0,
+                    "title": "Degree 0",
+                  },
+                ].map((e) {
+                  final Degree value = e['value'] as Degree;
+                  final String title = e['title'] as String;
+                  return RadioListTile(
+                    value: value,
+                    groupValue: selectedValue,
+                    title: Text(title),
+                    onChanged: (degree) {
+                      if (degree != null) {
+                        setState(() {
+                          selectedValue = degree;
+                        });
+                      }
+                    },
+                  );
+                }).toList(),
+              ),
             ),
           ),
           actions: [
